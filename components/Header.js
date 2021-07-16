9592dbfb8bbd60d307ac834f16000c1cd99bbcfe
iamwales <adeogunolawale@gmail.com>
2021-07-16 20:12:31 +0100

    pushing graphql note application live

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { withRouter } from "next/router";
+import { Navbar } from "react-bootstrap";
+import LogoutBtn from "./Auth/Logout";
+
+const Header = ({ router: { pathname } }) => (
+  <Navbar className="justify-content-between navBar">
+    <Navbar.Brand className="navBrand">Graph Note Application</Navbar.Brand>
+    <Navbar.Collapse className="justify-content-end navContainer navButton">
+      <LogoutBtn />
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default withRouter(Header);
